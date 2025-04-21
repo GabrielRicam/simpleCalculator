0a05cdab55af9505ac6fdfadcc6c95b5b45fe3f4
GabrielRicam <gbrielwiranata@gmail.com>
2025-04-21 23:04:15 +0700

    Using array for storing number

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 const screen = document.getElementById("screen");
-const input = [];
+const operator = ['+', '-', '/', '*'];
+let text = [];
 let n = 0;
-let text = "";
-
+text[n] = "0";
 
 
 
 document.addEventListener("keydown", function (pressed) {
-    if (!isNaN(pressed.key) && pressed.key.length === 1) {
-        if (text.length < 14) {
-            if (text === "0") text = "";
-            text += pressed.key;
-            screen.textContent = text;
+    
+    if (!isNaN(pressed.key) && pressed.key.length === 1)  {
+        if (text[n].length < 14 && !operator.includes(text[n].charAt(text[n].length - 1))) {
+            if (text[n] === "0") text[n] = "";
+            text[n] += pressed.key;
+            screen.textContent = text[n];
+        } else if (operator.includes(text[n].charAt(text[n].length - 1))){
+            console.log(text[n]);
+            
+            n++;
+            text[n] = "0";
         } else {
             screen.style.color = "rgb(255, 88, 88)";
             setTimeout(() => {
@@ -19,39 +25,72 @@ document.addEventListener("keydown", function (pressed) {
                 }, 200);
         }
     } else if (pressed.key === "Backspace") {
-        text = text.slice(0, -1);
-        if (text === "") text = "0";
+        text[n] = text[n].slice(0, -1);
+        if (text[n] === "") text = "0";
         screen.textContent = text;
     } else if (pressed.key === "Escape") {
-        text = "0";
-        screen.textContent = text;
+        text[n] = "0";
+        screen.textContent = text[n];
+    } else if (operator.includes(pressed.key) && text[n].length != 1) {
+        if (text[n].length < 14 && !operator.includes(text[n].charAt(text[n].length - 1))) {
+            if (text[n] === "0") text[n] = "";
+            text[n] += pressed.key;
+            screen.textContent = text[n];
+        } else {
+            screen.style.color = "rgb(255, 88, 88)";
+            setTimeout(() => {
+                screen.style.color = "";
+                }, 200);
+        }
+        
     }
 })
 
 document.querySelectorAll(".b-n").forEach(function(el) {
   el.addEventListener("click", function() {
-    if (text.length < 14) {
-        if (text === "0") text = "";
-        text += this.textContent;
-        screen.textContent = text;
-    } else {
-        screen.style.color = "rgb(255, 88, 88)";
-        setTimeout(() => {
-            screen.style.color = "";
-            }, 200);
-    }
-    
+    if (text[n].length < 14 && !operator.includes(text[n].charAt(text[n].length - 1))) {
+            if (text[n] === "0") text[n] = "";
+            text[n] += this.textContent;
+            screen.textContent = text[n];
+        } else if (operator.includes(text[n].charAt(text[n].length - 1))){
+            n++;
+            text[n] = "0";
+            screen.textContent = text[n];
+        } else {
+            screen.style.color = "rgb(255, 88, 88)";
+            setTimeout(() => {
+                screen.style.color = "";
+                }, 200);
+        }
   });
 });
 
+document.querySelectorAll(".b-a").forEach(function(el) {
+    el.addEventListener("click", function() {
+        if (text[n].length < 14 && !operator.includes(text[n].charAt(text[n].length - 1))) {
+            if (text[n] === "0") text[n] = "";
+            if (this.textContent === "x") {
+                text[n] += "*"
+            } else {
+                text[n] += this.textContent;
+            }
+            screen.textContent = text[n];
+        } else {
+            screen.style.color = "rgb(255, 88, 88)";
+            setTimeout(() => {
+                screen.style.color = "";
+                }, 200);
+        }
+    });
+});
 document.getElementById("delete").addEventListener("click", function() {
-    text = text.slice(0, -1);
-    if (text === "") text = "0";
-    screen.textContent = text;
+    text[n] = text[n].slice(0, -1);
+    if (text[n] === "") text[n] = "0";
+    screen.textContent = text[n];
 })
 
 document.getElementById("reset").addEventListener("click", function() {
-    text = "0";
-    screen.textContent = text;
+    text[n] = "0";
+    screen.textContent = text[n];
 })
 
