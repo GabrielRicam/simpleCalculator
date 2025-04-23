47f840aa9fed3c22d3c9f1781f6f3f28311f5094
GabrielRicam <gbrielwiranata@gmail.com>
2025-04-23 19:10:54 +0700

    Using array succeed

--- script.js
+++ script.js
@@ -8,16 +8,16 @@ text[n] = "0";
 
 // Keyboard Input
 document.addEventListener("keydown", function (pressed) {
-    if (!isNaN(pressed.key) && pressed.key.length === 1)  { //Detecting number input
+    if (
+        !isNaN(pressed.key) 
+        && pressed.key.length === 1
+        && pressed.key != " "
+    )  { //Detecting number input
         if (text[n].length < 14 && !operator.includes(text[n].charAt(text[n].length - 1))) {
             if (text[n] === "0") text[n] = "";
+            if (n != 0) history.textContent = text[n-1];
             text[n] += pressed.key;
             screen.textContent = text[n];
-            
-        } else if (operator.includes(text[n].charAt(text[n].length - 1))){  //Making new array for another operator
-            history.textContent = text[n];
-            n++;
-            text[n] = "0";
         } else { // Character exceed
             screen.style.color = "rgb(255, 88, 88)";
             setTimeout(() => {
@@ -25,24 +25,45 @@ document.addEventListener("keydown", function (pressed) {
                 }, 200);
         }
     } else if (pressed.key === "Backspace") { //Delete
-        text[n] = text[n].slice(0, -1);
+        if (text[n] != "0") {
+            text[n] = text[n].slice(0, -1);
         if (text[n] === "") text = "0";
         screen.textContent = text;
+        }
+        
     } else if (pressed.key === "Escape") { //Reset
+        n = 0;
         text[n] = "0";
         screen.textContent = text[n];
-    } else if (operator.includes(pressed.key) && text[n].length != 1) { //Operator
+        history.textContent = " ";
+    } else if (operator.includes(pressed.key) && text[n] != "0") { //Operator
         if (text[n].length < 14 && !operator.includes(text[n].charAt(text[n].length - 1))) {
             if (text[n] === "0") text[n] = "";
             text[n] += pressed.key;
             screen.textContent = text[n];
+
+            n++;
+            text[n] = "";
         } else { //Operator overlap
             screen.style.color = "rgb(255, 88, 88)";
             setTimeout(() => {
                 screen.style.color = "";
                 }, 200);
         }
-        
+    } else if (pressed.key === "=") { //Equal
+        if (n != 0 && text[n] != "") {
+            let result = eval(text.join(""));
+            console.log(text.join("") + " = " + result);
+            n = 0;
+            text[n] = result;
+            screen.textContent = text[n];
+            history.textContent = " ";
+        } else { // Not enough operator
+            screen.style.color = "rgb(255, 88, 88)";
+            setTimeout(() => {
+            screen.style.color = "";
+            }, 200);
+        } 
     }
 })
 
@@ -50,25 +71,24 @@ document.addEventListener("keydown", function (pressed) {
 document.querySelectorAll(".b-n").forEach(function(el) {
   el.addEventListener("click", function() {
     if (text[n].length < 14 && !operator.includes(text[n].charAt(text[n].length - 1))) {
-            if (text[n] === "0") text[n] = "";
-            text[n] += this.textContent;
-            screen.textContent = text[n];
-        } else if (operator.includes(text[n].charAt(text[n].length - 1))){ //Making new array for another operator
-            n++;
-            text[n] = "0";
-            screen.textContent = text[n];
-        } else {  //Character exceed
-            screen.style.color = "rgb(255, 88, 88)"; 
-            setTimeout(() => {
-                screen.style.color = "";
-                }, 200);
-        }
+        if (text[n] === "0") text[n] = "";
+        if (n != 0) history.textContent = text[n-1];
+        text[n] += this.textContent;
+        screen.textContent = text[n];
+    } else {  //Character exceed
+        screen.style.color = "rgb(255, 88, 88)"; 
+        setTimeout(() => {screen.style.color = "";}, 200);
+    }
   });
 });
 
 document.querySelectorAll(".b-a").forEach(function(el) { // Operator
     el.addEventListener("click", function() {
-        if (text[n].length < 14 && !operator.includes(text[n].charAt(text[n].length - 1))) {
+        if (
+            text[n].length < 14
+            && !operator.includes(text[n].charAt(text[n].length - 1))
+            && text[n] != "0"
+        ) {
             if (text[n] === "0") text[n] = "";
             if (this.textContent === "x") {
                 text[n] += "*"
@@ -76,14 +96,15 @@ document.querySelectorAll(".b-a").forEach(function(el) { // Operator
                 text[n] += this.textContent;
             }
             screen.textContent = text[n];
+            n++;
+            text[n] = "";
         } else { //Operator overlap
             screen.style.color = "rgb(255, 88, 88)";
-            setTimeout(() => {
-                screen.style.color = "";
-                }, 200);
+            setTimeout(() => {screen.style.color = "";}, 200);
         }
     });
 });
+
 document.getElementById("delete").addEventListener("click", function() { //Delete
     text[n] = text[n].slice(0, -1);
     if (text[n] === "") text[n] = "0";
@@ -91,7 +112,24 @@ document.getElementById("delete").addEventListener("click", function() { //Delet
 })
 
 document.getElementById("reset").addEventListener("click", function() { //Reset
+    n = 0;
     text[n] = "0";
     screen.textContent = text[n];
+    history.textContent = " ";
 })
 
+document.getElementById("equal").addEventListener("click", function() { //Equal
+    if (n != 0 && text[n] != "") {
+        let result = eval(text.join(""));
+        console.log(text.join("") + " = " + result);
+        n = 0;
+        text[n] = result;
+        screen.textContent = text[n];
+        history.textContent = " ";
+    } else { // Not enough operator
+        screen.style.color = "rgb(255, 88, 88)";
+        setTimeout(() => {
+        screen.style.color = "";
+        }, 200);
+    } 
+})
